672d366b979da68f787fcbb7b82665b858879110
Hugo Minzoni <50162408+hugominzoni@users.noreply.github.com>
2023-11-16 20:26:15 -0500

    initial commit

---
+++ js/router.js
@@ -0,0 +1,25 @@
+export class Router{
+    routes = {}
+
+    add(routeName, page){
+        this.routes[routeName] = page
+    }
+
+    route(e){
+        e = e || window.event
+        e.preventDefault()
+        window.history.pushState({},"", e.target.href)
+    
+        this.handle()
+    }
+
+    handle(){
+        const {pathname} = window.location
+        const route = this.routes[pathname] || this.routes[404]
+        fetch(route)
+        .then(data => data.text())
+        .then(html => {
+            document.querySelector('#app').innerHTML = html
+        } )
+    }
+}
